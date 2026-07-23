e533a965d53ddc9c09c578d69c1b2a42c5729dc7
leoseis <eboscrib@yahoo.com>
2026-07-23 13:39:09 +0100

    added a new field picture and phome nos

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,3 +6,28 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Church Profile",
+            {
+                "fields": (
+                    "phone_number",
+                    "profile_picture",
+                ),
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Church Profile",
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "phone_number",
+                    "profile_picture",
+                ),
+            },
+        ),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
+    phone_number = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/",
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.username
